4691df0079a7f629e2578f6958a071f3ede11114
Lementa <lementa.mari@gmail.com>
2026-06-04 21:21:02 +0300

    Add main in GUI class

--- gui.py
+++ gui.py
@@ -3,3 +3,18 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 from game_logic import MemoryGame
 
+class MemoryGameGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Гра тренування пам'яті")
+        self.root.geometry("750x650")
+        self.bg_color = "#dceefb"
+        self.game = None
+        self.buttons = []
+        self.seconds = 0
+        self.timer_running = False
+        self.is_paused = False
+        self.loaded_images = {}
+        self.main_frame = tk.Frame(self.root, bg=self.bg_color)
+        self.main_frame.pack(fill="both", expand=True)
+        self.show_menu()
